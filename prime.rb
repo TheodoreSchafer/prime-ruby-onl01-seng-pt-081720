6c00e7722cd49329cfe2b7b10f749ded2e38a16d
Theodore Schafer <theodore.schafer@gmail.com>
2020-08-26 19:44:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,20 +1,21 @@
 require 'prime'
-
 def prime?(num)
   range = (1..105557).to_a
   range.select do |numbers|
     numbers.prime?
+    if false
   end
  return true
 end
 
+
 def prime?(number)
   return false if !number.integer?
-
+#false
   return false if number < 2
-
+#false
   return true if number == 2
-
+#false
   (2..number-1).each {|int| return false if number % int == 0}
 
   true
